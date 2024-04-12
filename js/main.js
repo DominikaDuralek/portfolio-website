2e5c13c4154fcfdb9ac5bc2838de6370a483e9a8
DominikaDuralek <dominikaduralek@gmail.com>
2024-04-12 23:06:12 +0200

    test commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+import '../css/modern-normalize.css';
+import '../css/style.css';
+import '../css/utils.css';
+
+import '../css/components/header.css';
+import '../css/components/hero.css';
+import '../css/components/about.css';
+import '../css/components/skills.css';
+import '../css/components/projects.css';
+import '../css/components/contact.css';
+import '../css/components/footer.css';
