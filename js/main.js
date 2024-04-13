52b2bb6d92d979f767486acb7a6aa8120bad5c5c
DominikaDuralek <dominikaduralek@gmail.com>
2024-04-13 23:31:43 +0200

    minor changes

--- js/main.js
+++ js/main.js
@@ -9,3 +9,24 @@ import '../css/components/skills.css';
 import '../css/components/projects.css';
 import '../css/components/contact.css';
 import '../css/components/footer.css';
+
+// Get the button
+let buttonUp = document.querySelector('.button-up')
+buttonUp.addEventListener("click", goUp);
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollPage()};
+
+function scrollPage() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+    buttonUp.style.display = "block";
+  } else {
+    buttonUp.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function goUp() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
